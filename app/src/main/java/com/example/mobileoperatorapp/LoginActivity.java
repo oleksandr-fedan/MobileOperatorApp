5e6d09e2dec39756063fb47ba7b58e9171703970
Feduk <my.main.mail.mops@gmail.com>
2023-04-01 21:43:17 +0300

    Додана основна активність, яка містить чотири сторінки: "Профіль" "Активність" "Тарифи" "Сервіси"
    Налаштована навігація між сторінками через NavigationBar
    Додано 4 відповідних фрагменти до кожної сторінки
    НавБар стилізований не до кінця

--- app/src/main/java/com/example/mobileoperatorapp/LoginActivity.java
+++ app/src/main/java/com/example/mobileoperatorapp/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.mobileoperatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button button =findViewById(R.id.activity_login__bt);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
---
+++ app/src/main/java/com/example/mobileoperatorapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.mobileoperatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+}
