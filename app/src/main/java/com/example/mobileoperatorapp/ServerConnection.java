e27731509fb6654849b879f65aa623f5abe4b5da
Feduk <my.main.mail.mops@gmail.com>
2023-05-04 22:01:01 +0300

    Додано методи для отримання користувача з сервера за номером телефону та отримання списку тарифів

--- app/src/main/java/com/example/mobileoperatorapp/ServerConnection.java
+++ app/src/main/java/com/example/mobileoperatorapp/ServerConnection.java
@@ -1,20 +1,27 @@
 package com.example.mobileoperatorapp;
 
+import com.example.mobileoperatorapp.models.TariffModel;
+import com.example.mobileoperatorapp.models.UserModel;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
 public class ServerConnection {
-    private final String endPoint = "http://192.168.0.104:12531/";
+    private final String endPoint = "http://192.168.0.107:12531/";
 
     public String getLoginCode(String userPhoneNumber) {
         HttpURLConnection connection = null;
         String code = null;
 
         try {
-            connection = (HttpURLConnection) (new URL(endPoint + "api/user/" + userPhoneNumber).openConnection());
+            connection = (HttpURLConnection) (new URL(endPoint + "api/login/" + userPhoneNumber).openConnection());
             connection.setRequestMethod("GET");
 
             BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
@@ -31,7 +38,7 @@ public class ServerConnection {
         HttpURLConnection connection = null;
         boolean result = false;
         try {
-            connection = (HttpURLConnection) (new URL(endPoint + "api/user/" + userPhoneNumber + " " + code).openConnection());
+            connection = (HttpURLConnection) (new URL(endPoint + "api/login/" + userPhoneNumber + " " + code).openConnection());
             connection.setRequestMethod("GET");
 
             BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
@@ -42,5 +49,58 @@ public class ServerConnection {
         }
 
         return result;
+    }
+
+    public UserModel getUser(String userPhoneNumber) {
+        HttpURLConnection connection;
+        UserModel user = null;
+        try {
+            connection = (HttpURLConnection) (new URL(endPoint + "api/user/" + userPhoneNumber).openConnection());
+            connection.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            StringBuilder response = new StringBuilder();
+            while ((line = in.readLine()) != null) {
+                response.append(line);
+            }
+            in.close();
+            String json = response.toString();
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+            user = objectMapper.readValue(json, UserModel.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+
+    public List<TariffModel> getTariffs() {
+        HttpURLConnection connection;
+
+        List<TariffModel> tariffs = null;
+        try {
+            connection = (HttpURLConnection) (new URL(endPoint + "api/tariff/get_tariffs").openConnection());
+            connection.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            StringBuilder response = new StringBuilder();
+            while ((line = in.readLine()) != null) {
+                response.append(line);
+            }
+            in.close();
+            String json = response.toString();
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            tariffs = objectMapper.readValue(json, new TypeReference<List<TariffModel>>() {
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return tariffs;
     }
 }
